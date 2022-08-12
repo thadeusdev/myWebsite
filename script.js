54d8d940c9da9276d24d75e1705d9c5f9709d8a9
thadeus <thadeusnyariki94@gmail.com>
2022-08-12 05:13:53 +0300

    linked google form to website form field

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ function openmenu(){
 function closemenu(){
     sideMenu.style.right = "-200px";
 }
+
+// google contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxOXWsmYvvqBSYCQI4bovosGwJFkk893_BTByp5DMW6mUUJFGxIP025-CRfzxbHFLLOxQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)        
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+})
